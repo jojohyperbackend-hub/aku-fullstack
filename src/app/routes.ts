a641d959c3fb693495f88055256c914f80c704a9
Izzi Van Satoru <jauzacodingmaster@gmail.com>
2025-12-16 15:54:37 +0700

    revisi mas path nya v1

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,5 @@ import IndexPage from './pages/index.page';
 import AuthPage from './pages/auth.page';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' }, // root redirect ke auth
-  { path: 'auth', component: AuthPage },              // halaman login
-  { path: 'index', component: IndexPage },            // halaman utama setelah login
-];
+  { path: '', redirectTo: 'auth', pathMatch: 'full' }, // root diarahkan ke auth/
+  { path: 'auth', component:
